6851e7047d4d426c1ab9e022c5bc0d0e50862331
Gustavo Del Coli <gustavodelcoli@hotmail.com>
2023-01-20 16:25:29 -0300

    Fix Header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -10,7 +10,7 @@ function Header() {
                     <h1>DSMeta</h1>
                     <p>
                         Desenvolvido por
-                        <a href="https://github.com/Gustavo-DelColi">Github Gustavo-DelColi</a>
+                        <a href="https://github.com/Gustavo-DelColi"> Github Gustavo-DelColi</a>
                     </p>
             </div>
         </header>
